55560746f8acf5a78da24b705f9ec48fd1881260
Raybod-Code <137321279+Raybod-Code@users.noreply.github.com>
2026-04-17 19:18:56 +0330

    fix: add datasource url to prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,4 +3,7 @@ import path from 'path';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
+  datasource: {
+    url: `file:${path.resolve(process.cwd(), 'prisma', 'dev.db')}`,
+  },
 });
